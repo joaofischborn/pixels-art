ffbe0c40f24852e8d3ac10d3cb4f453cb54c5766
João Vitor <joaovitorfischborn@icloud.com>
2022-04-25 11:38:03 -0300

    finaliza exercicio 8

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-function selectColor(){
-    const colors = document.querySelectorAll('.color')
+const color1 = document.getElementById('black').style.backgroundColor = 'black'
+const color2 = document.getElementById('salmon').style.backgroundColor = 'salmon'
+const color3 = document.getElementById('green').style.backgroundColor = 'green'
+const color4 = document.getElementById('yellow').style.backgroundColor = 'yellow'
+
+const colors = document.querySelectorAll('.color')
     for(let i = 0; i < colors.length; i += 1){  
         colors[i].addEventListener('click', function(){
             const colorSelected = document.querySelector('.selected');
@@ -7,5 +11,11 @@ function selectColor(){
             colors[i].className = 'color selected'
         })
     }
-}
-selectColor()
+
+const selectPixels = document.querySelectorAll('.pixel')  
+    for(let i = 0; i < selectPixels.length; i += 1){
+        selectPixels[i].addEventListener('click', function(){
+            const selectedClass = document.querySelector('.selected')
+            selectPixels[i].style.backgroundColor = selectedClass.style.backgroundColor
+        })
+    }
